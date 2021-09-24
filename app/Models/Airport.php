7a2fe95d7c4ce229e1f8fcdaa6643e3520b5ae1d
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2021-09-24 18:25:56 +0300

    making relationship for thr Airport model

--- app/Models/Airport.php
+++ app/Models/Airport.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Airport extends Model
 {
     use HasFactory;
+
+    public function arrivingFlights(){
+        return $this->hasany('App\Flight','arrivalAirport_id');
+    }
+
+    departingFlights(){
+        return $this->hasany('App\Flight','departureAirport_id');
+    }
+
 }
