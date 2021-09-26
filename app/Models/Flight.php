359a115de8959066a5004785730951c6dda3b4f1
Oleksii Derzhuk <oleksii.derzhuk@gmail.com>
2021-09-26 11:35:09 +0300

    making relationship for the Flight model

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Flight extends Model
 {
     use HasFactory;
+
+    public function arrivalAirport(){
+        return $this->belongsTo('App\Airport', 'arrivalAirport_id');
+    }
+
+    public function departureAirport(){
+        return $this->belongsTo('App\Airport', 'departureAirport_id');
+    }
+
+    public function passengers(){
+        return $this->belongsToMany('App\Customer', 'flight_customer');
+    }
 }
